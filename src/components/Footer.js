564b838c86656548ea5cdc5154b489397d927f44
papeparrot <papeparrot@gmail.com>
2023-02-27 01:32:17 +0900

    feat: footer 선 길이 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="mt-8 border-t-2 border-gray-300 pt-4 text-center w-7/12 mx-auto border-solid">
+    <div className="mt-8 border-t-2 border-gray-300 pt-4 text-center w-8/12 mx-auto border-solid">
       <p className="text-gray-500 mt-5">
         Copyright ©️ Mofit. All right reserved.
       </p>
