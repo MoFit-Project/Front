3213aa6b1fe7f372cb18653fef029775b582c674
Woo <116056778+woe3@users.noreply.github.com>
2023-03-09 01:17:46 +0900

    싱글모드 페이지 로그인 접근 추가

--- src/pages/single-mode.js
+++ src/pages/single-mode.js
@@ -2,6 +2,7 @@ import Loading from '../components/Loading'
 import SingleWebcam from '../components/SingleWebcam'
 import { useState } from 'react';
 import dynamic from "next/dynamic";
+import LayoutAuthenticated from "../components/LayoutAuthticated";
 
 const DynamicComponentWithNoSSR = dynamic(
     () => import('../components/SingleGame/Index'),
@@ -14,6 +15,7 @@ export default function SingleMode() {
     const [startDetect, setStartDetect] = useState(false);
     return (
         <>
+            <LayoutAuthenticated>
             <title>MOFIT 게임룸</title>
             <div className="loading">
                 <Loading/>
@@ -47,6 +49,7 @@ export default function SingleMode() {
                 }
 
                 `}</style>
+            </LayoutAuthenticated>
         </>
 
     )
